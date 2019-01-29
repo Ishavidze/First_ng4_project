2d7b95db768f300286688dbdfbec9c70e458f9b5
Ishavidze <iago.shavidze@wandio.com>
2019-01-29 20:12:55 +0400

    add-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeListRecipeItemComponent } from './recipes/recipe-list-recipe-item/recipe-list-recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+    RecipeListRecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipe-list-recipe-item/recipe-list-recipe-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list-recipe-item',
+  templateUrl: './recipe-list-recipe-item.component.html',
+  styleUrls: ['./recipe-list-recipe-item.component.css']
+})
+export class RecipeListRecipeItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
